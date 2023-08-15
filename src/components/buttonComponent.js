8b2dc76135402f590429cc482515b4ebe99cd5c4
Santiago Servera <santiagoservera21@gmail.com>
2023-08-15 17:14:54 -0300

    Resolved conflicts

---
+++ src/components/buttonComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './buttonComponent.css';
+const ButtonComponent = () => {
+    return (
+      <button className="custom-button">
+        Boton sin funcionalidad 
+      </button>
+    );
+  };
+  
+  export default ButtonComponent;
